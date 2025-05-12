b478b0d8381b3f70740996eca1ecfd2e70e445eb
kxshiii <brian.gikaru@student.moringaschool.com>
2025-05-12 12:11:04 +0300

    comm

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python31
 
 class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
+fido= Person()
+fido.talk()
+fido.walk()
+
